be4fbf173d85188fc9cc3c72a66c30859da5eb3a
zy <13671215329@163.com>
2018-11-13 10:40:05 +0800

    本地、私服、bintray有问题

---
+++ app/src/main/java/cn/zhangyong/mavenlearn/Test.java
@@ -0,0 +1,14 @@
+package cn.zhangyong.mavenlearn;
+
+/**
+ * description:
+ * autour: zhangyong
+ * date: 2018/11/12 16:08
+ * update: 2018/11/12
+ * version:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ bintraylibrary/src/main/java/cn/zhangyong/bintraylibrary/BintrayLearn.java
@@ -0,0 +1,14 @@
+package cn.zhangyong.bintraylibrary;
+
+/**
+ * description:
+ * autour: zhangyong
+ * date: 2018/11/12 15:59
+ * update: 2018/11/12
+ * version:
+ */
+public class BintrayLearn {
+    public static String bintrayLearnHello(){
+        return "hello,bintray!";
+    }
+}
